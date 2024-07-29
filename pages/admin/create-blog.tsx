ceca46130aa8d2cc2aa4029c6aace57d47e40606
XTDV2021 <xuanthudoanvo@gmail.com>
2024-07-30 00:01:02 +0700

    commit
    update

--- pages/admin/create-blog.tsx
+++ pages/admin/create-blog.tsx
@@ -1,9 +1,11 @@
 import { AdminLayout, CreateBlog } from "@/app/components";
+//comment
 
 export default function PostManagementPage() {
     return (
         <AdminLayout>
             <CreateBlog />
         </AdminLayout>
+        
     )
 }
